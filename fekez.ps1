028b08c8777f581de842049b901e4c8363aa5ed7
yaayimded <143564303+yaayimded@users.noreply.github.com>
2024-12-08 20:09:01 +0100

    Update fekez.ps1

--- fekez.ps1
+++ fekez.ps1
@@ -1,24 +1,17 @@
-# Szkript neve: fekez.ps1
-# Használat: .\fekez.ps1 <K>
-# Példa: .\fekez.ps1 100
-
 param (
-    [int]$K  # A távolságérték, amely alatt fékezni kell
+    [int]$K
 )
 
 $adatFajl = ".\adatok.txt"
 
-# Fájl feldolgozása
 Get-Content $adatFajl | ForEach-Object {
-    # Sor feldolgozása
-    $sor = $_ -split ",\s*"  # A sor elemeit vesszővel választjuk el
-    $idopont = $sor[0]       # Időpont
-    $sebesseg = $sor[1]      # Sebesség
-    $tavolsagElol = $sor[2]  # Távolság elöl
-    $tavolsagHatul = $sor[3] # Távolság hátul
+    $sor = $_ -split ",\s*"
+    $idopont = $sor[0]
+    $v = $sor[1]
+    $elol = $sor[2]
+    $hatul = $sor[3] 
 
-    # Fékezési feltétel
-    if ([int]$tavolsagElol -lt $K) {
-        Write-Output "Fékezni kell! Időpont: $idopont, Távolság elöl: $tavolsagElol"
+    if ([int]$elol -lt $K) {
+        Write-Output "fekez! ido: $idopont, tav elol: $elol"
     }
 }
